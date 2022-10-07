2235e91f9a5305d45389bcc951e57efafe4030b4
omarsaeed2466 <47633542+omarsaeed2466@users.noreply.github.com>
2022-10-07 18:50:38 +0200

    improve readibility

--- src/Main/Java/Plateau.java
+++ src/Main/Java/Plateau.java
@@ -1,13 +1,28 @@
 package Main.Java;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Plateau {
     public int dimX ;
     public int dimY ;
-
+private List<Rover> rovers = new ArrayList<Rover>();
     public Plateau(int dimX, int dimY) {
         this.dimX = dimX;
         this.dimY = dimY;
+    }
+    public void addRover(Rover rover){
+rovers.add(rover);
+    }
+
+
+    public boolean isOccupied(Position position){
+        for (Rover r : rovers){
+            if (r.hasPosition(position)){
+                return true ;
+
+            }
+        }
+        return false ;
     }
 }
--- src/Main/Java/Rover.java
+++ src/Main/Java/Rover.java
@@ -9,6 +9,9 @@ public class Rover {
     public Rover(String name) {
         this.name = name;
     }
+
+
+
     public void dropRover(Plateau plateau , String args){
         String[] parts = args.split(" ");
         int x = Integer.parseInt(parts[0]);
@@ -17,9 +20,111 @@ public class Rover {
         dropRover(plateau,new Position(x,y),heading);
     }
 
-    private void dropRover(Plateau plateau, Position position, Heading heading) {
+    public void dropRover(Plateau plateau, Position position, Heading heading) {
         if (!position.IsOnPlateau(plateau)){
             throw new PositionNotOnPlateauException(plateau,position);
-        }if (plateau.)
+        }if (plateau.isOccupied(position)){
+            throw new RuntimeException("Already occupied by a rover!");
+        }
+        this.plateau = plateau;
+        this.position = position;
+        this.heading = heading;
+
+        plateau.addRover(this);
+    }
+
+
+    public void dropRover(Plateau plateau , int posX , int posY , char heading){
+        dropRover(plateau,new Position(posX,posY),ToHeading(heading));
+    }
+
+
+
+
+
+         public String reportStatus(){
+StringBuilder sb = new StringBuilder(name);
+sb.append("");
+sb.append(reportStatus());
+return sb.toString();
+}
+
+
+    public String reportPosition() {
+        if (position == null || heading == null) {
+            return "Not dropped yet.";
+        }
+        return position.toString() + " " + FromHeading(heading);
+    }
+
+
+    public boolean hasPosition(Position pos) {
+        return position.isEqual(pos);
+    }
+
+
+
+    public void processInstruction(Instruction[] instruction){
+        for (Instruction i : instruction){
+            processInstruction(i);
+        }
+    }
+    private void processInstruction(Instruction instruction){
+        if (position == null || heading == null) {
+            throw new NotDroppedException();
+        }
+
+        switch (instruction) {
+            case LEFT: turnLeft(); break;
+            case MOVE: moveForward(); break;
+            case RIGHT: turnRight(); break;
+            default: throw new RuntimeException("Should not get here!");
+        }
+    }
+
+    private void turnLeft() {
+        switch (heading) {
+            case EAST: heading = Heading.NORTH; break;
+            case NORTH: heading = Heading.WEST; break;
+            case SOUTH: heading = Heading.EAST; break;
+            case WEST: heading = Heading.SOUTH; break;
+            default: throw new RuntimeException("Should not get here!");
+        }
+    }
+
+    private void turnRight() {
+        switch (heading) {
+            case EAST: heading = Heading.SOUTH; break;
+            case NORTH: heading = Heading.EAST; break;
+            case SOUTH: heading = Heading.WEST; break;
+            case WEST: heading = Heading.NORTH; break;
+            default: throw new RuntimeException("Should not get here!");
+        }
+    }
+
+    private void moveForward() {
+        Position newPosition = position.moveForward(heading);
+        if (!newPosition.IsOnPlateau(plateau)) {
+            throw new PositionNotOnPlateauException(plateau, newPosition);
+        }
+        position = newPosition;
+    }
+    private static Heading ToHeading(char heading) {
+        switch (heading) {
+            case 'N': return Heading.NORTH;
+            case 'W': return Heading.WEST;
+            case 'S': return Heading.SOUTH;
+            case 'E': return Heading.EAST;
+            default: throw new RuntimeException("Unsupported character '" + heading + "'!");
+        }
+    }
+    private static char FromHeading(Heading heading) {
+        switch (heading) {
+            case NORTH: return 'N';
+            case WEST: return 'W';
+            case SOUTH: return 'S';
+            case EAST: return 'E';
+            default: throw new RuntimeException("Unsupported heading '" + heading + "'!");
+        }
     }
 }
